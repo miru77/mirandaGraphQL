d36cc2e5503994726806564fe3416e5cfe5a90ca
jorge miranda <jorgemiranda1977@gmail.com>
2020-07-02 16:49:18 -0300

    haciendo responsive tabla de productos

--- pages/productos.js
+++ pages/productos.js
@@ -31,7 +31,9 @@ const Productos = () => {
         <Link href="/nuevoproducto">
         <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">Nuevo Producto</a>
         
-        </Link>
+        </Link> 
+
+        <div className="overflow-x-scroll">
 
         <table className="table-auto shadow-md mt-10 w-full w-lg">
               <thead className="bg-gray-800">
@@ -52,7 +54,7 @@ const Productos = () => {
                 ))}
             </tbody>
           </table>
-
+          </div>
         </Layout>
       </div>
   )
